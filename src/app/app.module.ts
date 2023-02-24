0c0df603e1b7af9877ec49f306f7056b3590399e
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 06:39:12 +0700

    Creating navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
