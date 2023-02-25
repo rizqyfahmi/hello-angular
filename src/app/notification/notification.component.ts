35e2f038151cce16a94c071a28b8613f0e689e1a
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 08:01:58 +0700

    Property binding in template property

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success">
+  template: `<div class="alert alert-success" [hidden]="isHidden">
               <p>This website uses cookie to provide better user experience.</p>
             </div>`,
   styles: [".notification { margin: 10px 0px; padding: 10px 20px; background-color: #B4E4FF; text-align: center; }", "p { font-size: 14px }"] 
 })
 export class NotificationComponent {
-
+  isHidden: boolean = false
 }
