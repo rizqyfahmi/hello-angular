7eb4a8e79a4b72066ae2514fcbe57f6b48d14dfd
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 07:02:44 +0700

    Using template property in single line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
     ContainerComponent,
     NavComponent,
     HeaderComponent,
+    NotificationComponent,
   ],
   imports: [
     BrowserModule,
