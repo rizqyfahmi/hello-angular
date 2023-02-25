8e9fef970db032882ee4f561f12ac66e3d074c3d
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 23:29:26 +0700

    View encapsulation ShadowDom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None // None makes css applied for the child element of this element
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title: string = ""
