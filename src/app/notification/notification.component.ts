43a39c3a0afea7be3f6af0df75d7f3cbf73ed03b
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 07:11:57 +0700

    Applying class in styles property

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div>
+  template: `<div class="notification">
               <p>This website uses cookie to provide better user experience.</p>
             </div>`,
-  styles: ["div { margin: 10px 0px; padding: 10px 20px; background-color: #B4E4FF; text-align: center; }", "p { font-size: 14px }"] 
+  styles: [".notification { margin: 10px 0px; padding: 10px 20px; background-color: #B4E4FF; text-align: center; }", "p { font-size: 14px }"] 
 })
 export class NotificationComponent {
 
