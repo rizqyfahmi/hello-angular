cdd18c39a22d8f48c0354c93311d4cabe836c414
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 23:15:36 +0700

    View encapsulation Emulated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { DemoComponent } from './demo/demo.component';
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // Emulated is the default value
 })
 export class AppComponent {
-  /*
-    ViewChild is a way to pass element reference from view template into its class
-  */
-  // "dobInput" is a template reference variable
-  @ViewChild('dobInput') dateOfBirth!: ElementRef;
-  // "ageInput" is a template reference variable
-  @ViewChild('ageInput') age!: ElementRef;
-  // Custom component has its own way
-  @ViewChild(DemoComponent, { static: true }) demoComponent!: DemoComponent;
-
-  onCalculateAge() {
-    const value = this.dateOfBirth.nativeElement.value;
-    if (!value) return;
-
-    const birthYear = new Date(value).getFullYear()
-    const currentYear = new Date().getFullYear();
-
-    this.age.nativeElement.value = currentYear - birthYear;
-    
-  }
+  title: string = ""
 }
