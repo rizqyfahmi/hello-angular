c4ba72b3c12bdf9ca8d51d16da5873d398361d03
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 07:04:55 +0700

    Using template property in multiple line

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: '<div><p>This website uses cookie to provide better user experience.</p></div>',
+  template: `<div>
+              <p>This website uses cookie to provide better user experience.</p>
+            </div>`,
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent {
