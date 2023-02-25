638b04362f2c96284a824d501f59b095e35bc845
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 07:27:28 +0700

    Using selector as html attribute

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: '[app-nav]',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
