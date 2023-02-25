7e236f11c9d250aeed7a11f424df7f08467800c0
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-25 08:29:19 +0700

    Event binding in template property

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -4,9 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-notification',
   template: `<div class="alert alert-success" [hidden]="isHidden">
               <p>This website uses cookie to provide better user experience.</p>
+              <div class="close"><button class="btn" (click)="onCloseNotification()">X</button></div>
             </div>`,
-  styles: [".notification { margin: 10px 0px; padding: 10px 20px; background-color: #B4E4FF; text-align: center; }", "p { font-size: 14px }"] 
+  styles: [
+    ".notification { margin: 10px 0px; padding: 10px 20px; background-color: #B4E4FF; text-align: center; }", 
+    "p { font-size: 14px; margin: 0px }",
+    ".close { position: absolute; right: 15px; top: 50%; transform: translateY(-50%); }"
+  ] 
 })
 export class NotificationComponent {
-  isHidden: boolean = false
+  
+  isHidden: boolean = false;
+
+  onCloseNotification() {
+    this.isHidden = true;
+  }
+
 }
