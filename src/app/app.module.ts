0ae6573b7c0b9432f43602d491665079bc42f124
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-26 22:03:18 +0700

    Directive: reorganized setBackground

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SetBackgroundDirective } from './set-background.directive';
+import { SetBackgroundDirective } from './directives/set-background/set-background.directive';
 
 @NgModule({
   declarations: [
