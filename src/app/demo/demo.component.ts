96075b6b64a42136d00cbc0940c67d3301de39d7
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-26 11:26:33 +0700

    Lifecycle: ngOnInit

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,19 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent {
-  @Input() value: string = "Initial Value";
+export class DemoComponent implements OnInit {
+  @Input() value: string = "";
 
   /**
    * - It's not part of angular lifecycle, It's a feature from javascript/typescrypt
    * - Constructor will be executed at first time when class component is run (before angular lifecycle)
    */
   constructor() {
-    console.log("- Constructor called!");
+    console.log("- constructor called!");
+    console.log("  Value: ", this.value);
+  }
+
+  /**
+   * - ngOnInit gets fired only once just after the default change detector has checked for the first time
+   */ 
+  ngOnInit(): void {
+    console.log("- ngOnInit called!");
     console.log("  Value: ", this.value);
   }
 }
