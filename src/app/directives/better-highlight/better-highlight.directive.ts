80ed79157669c4e9aba87d9a724106c40f412233
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-19 00:53:16 +0700

    Directive: binding directive properties

--- src/app/directives/better-highlight/better-highlight.directive.ts
+++ src/app/directives/better-highlight/better-highlight.directive.ts
@@ -1,23 +1,37 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
+  // "@input" make "defaultColor" is assignable from the host element by using [defaultColor] = ""
+  @Input() defaultColor: string = "transparent";
+  // "@input" make "highlightColor" is assignable from the host element by using [highlightColor] = ""
+  @Input() highlightColor: string = "pink";
+
+  ngOnInit(): void {
+    /**
+     * - "defaultColor" is transparent as the default,
+     *   the new value of "defaultColor" that is assigned from the host element is available when it is initialized. 
+     *   So, we need to assign the value here 
+     * */
+    this.background = this.defaultColor;
+  }
+
   @HostBinding("style.backgroundColor")
-  background: string = "yellow"
+  background: string = this.defaultColor;
 
   @HostListener("mouseenter")
   mouseenter() {
-    this.background = "pink";
+    this.background = this.highlightColor;
   }
 
   @HostListener("mouseleave")
   mouseleave() {
-    this.background = "yellow"
+    this.background = this.defaultColor;
   }
 
 }
