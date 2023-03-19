8a9704eae473bcd66798bf2fca3edfea20235d3f
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-19 10:58:41 +0700

    Directive: custom appClass directive

---
+++ src/app/directives/class/class.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+
+  @Input() set display(value: Object) {
+    const entries = Object.entries(value);
+    for(let [className, condition] of entries) {
+      if (condition) {
+        this.renderer.addClass(this.element.nativeElement, className);
+      }
+    }
+  }
+}
