6c8c88a43773382773cd38d12d63ce1e85cb948e
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-19 11:06:19 +0700

    Directive: make an attribute selector without @input alias

--- src/app/directives/class/class.directive.ts
+++ src/app/directives/class/class.directive.ts
@@ -7,8 +7,8 @@ export class ClassDirective {
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
-  // When @Input alias is set, then we can't use "display"
-  @Input("appClass") set display(value: Object) {
+  // Make an attribute selector without @input alias
+  @Input() set appClass(value: Object) {
     const entries = Object.entries(value);
     for(let [className, condition] of entries) {
       if (condition) {
