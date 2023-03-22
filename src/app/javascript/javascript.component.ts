a99190894f2aa3f0e4beaa0a1eee7767cb07094a
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-22 23:39:49 +0700

    Service: creating a service
    Note: service is a reusable typescrypt class that can be used in multiple component across our Angular application

---
+++ src/app/javascript/javascript.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { EnrollService } from '../services/enroll/enroll.service';
+
+@Component({
+  selector: 'app-javascript',
+  templateUrl: './javascript.component.html',
+  styleUrls: ['./javascript.component.css']
+})
+export class JavascriptComponent {
+  title: string = "Javascript";
+
+  constructor(private enrollService: EnrollService) {}
+
+  onEnroll() {
+    this.enrollService.onEnrollClicked(this.title);
+  }
+}
