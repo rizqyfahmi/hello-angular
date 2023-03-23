c644709db9c45fa2d6a57a7226da9b5e3af4dbcf
Rizqy Fahmi <rizqyfahmi@yahoo.com>
2023-03-23 22:10:43 +0700

    Revert "Service: creating a service"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JavascriptComponent } from './javascript/javascript.component';
-import { AngularComponent } from './angular/angular.component';
-import { EnrollService } from './services/enroll/enroll.service';
+import { SetBackgroundDirective } from './directives/set-background/set-background.directive';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
+import { HoverDirective } from './directives/hover/hover.directive';
+import { BetterHighlightDirective } from './directives/better-highlight/better-highlight.directive';
+import { ClassDirective } from './directives/class/class.directive';
+import { StyleDirective } from './directives/style/style.directive';
+import { IfDirective } from './directives/if/if.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JavascriptComponent,
-    AngularComponent
+    SetBackgroundDirective,
+    HighlightDirective,
+    HoverDirective,
+    BetterHighlightDirective,
+    ClassDirective,
+    StyleDirective,
+    IfDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [EnrollService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
