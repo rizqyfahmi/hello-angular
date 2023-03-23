3a062e5f31f485c40a2dc629c05b6b1dae5404ee
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-23 23:18:54 +0700

    Service: hirarchical injection for a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { EnrollService } from './services/enroll/enroll.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /**
+   * - Once we provide a service on a component, the angular framework will create and inject an instance 
+   *   of that service for that component and all its child components
+   * */
+  providers: [EnrollService]
 })
 export class AppComponent {
   title = 'hello-angular';
