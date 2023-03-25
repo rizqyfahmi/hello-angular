a6848f522c8eb32a56f65b42c50c16389f1e6e7d
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-25 20:10:41 +0700

    Observable: unsubscribe an observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, from, map } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Observable';
+
+  /**
+   * - Create an observable that emits sequential numbers every specified time interval
+   * - The observable created by this interval function will keep on emitting the data indefinitely unless we unsubscribe that observable
+   * */ 
+  myObservable = interval(1000)
+  subscribedObservable?: Subscription;
+
+  ngOnInit(): void {
+    this.subscribe();
+  }
+
+  // It will create a new observable every time we call this method, even if we already have an active observable.
+  subscribe() {
+    this.subscribedObservable = this.myObservable.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  // It will stop subscription. (Only the latest observable, so that we need to prevent creating multiple observable)
+  unsubscribe() {
+    this.subscribedObservable?.unsubscribe();
+  }
 }
