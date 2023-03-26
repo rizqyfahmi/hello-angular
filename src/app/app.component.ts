11847048ebff9867aeda193453a0c52d648b0ac5
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-27 00:29:54 +0700

    Route: navigation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -9,12 +9,24 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit{
   title = 'hello-angular';
+  displayLoadingIndicator: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute, private authService: AuthService) {}
+  constructor(private activatedRoute: ActivatedRoute, private authService: AuthService, private route: Router) {}
   
   ngOnInit(): void {
     this.activatedRoute.fragment.subscribe((value) => {
       document.getElementById(value!)?.scrollIntoView({ behavior: 'smooth' })
+    });
+
+    this.route.events.subscribe((routerEvent: Event) => {
+      // Since we create a "resolve route guard", then we need to show loading bar when visit a page
+      if (routerEvent instanceof NavigationStart) {
+        this.displayLoadingIndicator = true;
+      }
+      
+      if (routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError) {
+        this.displayLoadingIndicator = false;
+      }
     })
   }
 
