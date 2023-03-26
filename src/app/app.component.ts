bc1d2f7a61a81fef6a8178d5d8f78a506105b6ba
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-26 11:02:56 +0700

    Route: passing fragment to route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hello-angular';
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe((value) => {
+      document.getElementById(value!)?.scrollIntoView({ behavior: 'smooth' })
+    })
+  }
+
 }
