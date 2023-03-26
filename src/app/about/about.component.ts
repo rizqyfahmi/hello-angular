e14c7309bea1dd6625b9501b107f56d12b563021
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-26 07:53:07 +0700

    Route: navigate programmatically using "navigateByUrl"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {}
+export class AboutComponent {
+
+  constructor(private route: Router) {}
+
+  navigateToHome() {
+    /**
+     * - By default, navigate uses absolute route paths
+     * - We need to pass url its argument
+     * */ 
+    this.route.navigateByUrl("Home");
+  }
+
+}
