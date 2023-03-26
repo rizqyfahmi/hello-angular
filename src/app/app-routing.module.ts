acf5d02a0efaf36848b855f7d2d11f29499807d7
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-26 12:44:01 +0700

    Route: separate the route file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CourseComponent } from './courses/course/course.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+  {
+    path: 'Courses', children: [
+      { path: '', component: CoursesComponent },
+      { path: 'Course/:id', component: CourseComponent }
+    ]
+  },
+  /**
+   * - This wild card route should be placed in the last, 
+   *   that's because this route matches to every route path which the user will enter in the url bar
+   * - So, angular will search the matched route from the top
+   * */
+  { path: "**", component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,11 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { CourseComponent } from './courses/course/course.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'Home', component: HomeComponent },
-  { path: 'About', component: AboutComponent },
-  { path: 'Contact', component: ContactComponent },
-  { path: 'Courses', children: [
-    { path: '', component: CoursesComponent },
-    { path: 'Course/:id', component: CourseComponent }
-  ]},
-  /**
-   * - This wild card route should be placed in the last, 
-   *   that's because this route matches to every route path which the user will enter in the url bar
-   * - So, angular will search the matched route from the top
-   * */ 
-  { path: "**", component: ErrorComponent } 
-]
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,7 +23,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
