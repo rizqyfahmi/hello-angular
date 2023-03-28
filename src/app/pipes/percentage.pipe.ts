7815b6f1df4817e784f711b2e83539df13e41ed2
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-28 23:49:47 +0700

    Pipe: creating a custom pipe

--- src/app/pipes/percentage.pipe.ts
+++ src/app/pipes/percentage.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PercentagePipe implements PipeTransform {
 
   transform(value: number, totalMarks: number, decimal: number): string {
-    console.log('percentage pipe is called!');
     return ((value / totalMarks) * 100).toFixed(decimal);
   }
 
