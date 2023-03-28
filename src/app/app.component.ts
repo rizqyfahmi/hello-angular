66a4655d8eb362ea273e17669c8ee475652e28ff
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-28 23:49:47 +0700

    Pipe: creating a custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './services/student.service';
+import { Student } from './types/student';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-angular';
+export class AppComponent implements OnInit{
+  title: string = 'Angular Pipes';
+  students: Student[] = []; 
+  totalMarks: number = 0
+
+  constructor(private studentService: StudentService) {}
+  
+  ngOnInit(): void {
+    this.students = this.studentService.students;
+    this.totalMarks = this.studentService.totalMarks;
+  }
 }
