32d6f91c5e4efb6cc53455d6dffaed82b40229c5
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-03-31 00:52:08 +0700

    Pipe: move filtering logic into component itself

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PercentagePipe } from './pipes/percentage.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PercentagePipe,
-    FilterPipe
   ],
   imports: [
     BrowserModule,
