54c2ae3c1b9f8b0d7ce560186af34dd05f197945
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-01 12:40:26 +0700

    Form: logging the form as html element using template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello-angular';
 
-  onSubmit() {
-    console.log('Form Submitted!')
+  onSubmit(form: HTMLFormElement) {
+    console.log(form)
   }
 }
