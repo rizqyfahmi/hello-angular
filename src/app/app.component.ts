d8d98044acde625fd7803ab740a53682680ca793
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-02 06:25:53 +0700

    ReactiveForm: creating reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-angular';
+export class AppComponent implements OnInit{
+  title = 'Reactive Form';
+  reactiveFormProps?: FormGroup;
+
+  ngOnInit(): void {
+    this.reactiveFormProps = new FormGroup({
+      firstname: new FormControl(null), // "null" is its default value
+      lastname: new FormControl(null), // "null" is its default value
+      email: new FormControl(null), // "null" is its default value
+      gender: new FormControl('male'), // "male" is its default value
+      country: new FormControl('usa'), // "usa" is its default value
+      hobbies: new FormControl(null), // "null" is its default value
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reactiveFormProps);
+  }
+
 }
