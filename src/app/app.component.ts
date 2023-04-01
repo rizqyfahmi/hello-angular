7e9e3fa1c34c7f74f3f28721ae66a7b8de5a7ae5
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-01 15:18:58 +0700

    Form: add default value for radio button using property binding in template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,13 @@ export class AppComponent {
   defaultCountry = 'india';
   firstname: string = '';
   lastname: string = '';
+  defaultGender: string = "Female";
+  genders: { id: string, value: string }[] = [
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Other' },
+  ];
+
   @ViewChild('myForm') form?: NgForm;
 
   onSubmit() {
