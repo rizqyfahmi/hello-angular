6f2021e52a800ee910375f2d7673198f0f0e3760
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-02 14:28:33 +0700

    HttpRequest: sending POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'Angular Http Request';
+
+  constructor(private http: HttpClient) {}
+
+  onProductCreate(product: { name: string, description: string, price: string }) {
+    console.log('product: ', product);
+    const headers = new HttpHeaders({
+      'Content-Type': ' application/json'
+    });
+    this.http.post('http://localhost:4000/products', product, { headers: headers }).subscribe((response) => {
+      console.log('response: ', response);
+      
+    });
+  }
 }
