c6ba42f8d4d772a534f10b793be51639cda34af1
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 09:59:59 +0700

    Ngrx: adding reducer into post store

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './components/posts/posts.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './stores/reducers';
 
 @NgModule({
   declarations: [
     PostsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature("posts", reducers)
   ],
   exports: [
     PostsComponent
