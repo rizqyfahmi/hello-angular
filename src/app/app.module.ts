0748ad0b1a55c0f3d2e8c6ef33a8f826a3604494
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 16:28:15 +0700

    Ngrx: connecting post effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PostsModule } from './posts/posts.module';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     AppRoutingModule,
     PostsModule,
     StoreModule.forRoot({}), // It is filled by empty object because we want to define all our reducers and related stuff inside that module
+    EffectsModule.forRoot(),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: true, // Restrict extension to log-only mode
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PostsComponent } from './components/posts/posts.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './stores/reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { PostsEffects } from './stores/effects';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { reducers } from './stores/reducers';
   ],
   imports: [
     CommonModule,
-    StoreModule.forFeature("posts", reducers) // "posts" will become a part of app state
+    StoreModule.forFeature("posts", reducers), // "posts" will become a part of app state
+    EffectsModule.forFeature([
+      PostsEffects
+    ])
   ],
   exports: [
     PostsComponent
