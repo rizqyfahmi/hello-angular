c169a87731f4434abfec528e9ee19ad386ba282e
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 08:48:32 +0700

    Ngrx: mocking api call by create an observable

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
+import { PostInterface } from '../types/posts.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  getPosts(): Observable<PostInterface[]> {
+    const posts: PostInterface[] = [
+      { id: "1", title: "First post" },
+      { id: "2", title: "Second post" },
+      { id: "3", title: "Third post" },
+    ];
+
+    return of(posts).pipe(delay(2000));
+  }
 }
