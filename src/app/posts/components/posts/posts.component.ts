706d97155984459b001ea3c47b42845c8f0981b7
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 09:30:22 +0700

    Ngrx: connecting post component into app component

--- src/app/posts/components/posts/posts.component.ts
+++ src/app/posts/components/posts/posts.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
