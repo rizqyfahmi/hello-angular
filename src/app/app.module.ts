2cdb8754ab2b7b82d61fd1f049796d7aeb124bcd
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 10:14:06 +0700

    Ngrx: adding StoreDevtoolsModule for debugging state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsModule } from './posts/posts.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { StoreModule } from '@ngrx/store';
     BrowserModule,
     AppRoutingModule,
     PostsModule,
-    StoreModule.forRoot({}) // It is filled by empty object because we want to define all our reducers and related stuff inside that module
+    StoreModule.forRoot({}), // It is filled by empty object because we want to define all our reducers and related stuff inside that module
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: true, // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
