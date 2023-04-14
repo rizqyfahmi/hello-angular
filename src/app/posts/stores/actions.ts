1060708ec284f72b8527475c85855a7d2c4ea63c
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 15:10:47 +0700

    Ngrx: prepare for ngrx effect

--- src/app/posts/stores/actions.ts
+++ src/app/posts/stores/actions.ts
@@ -1,3 +1,6 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
+import { PostInterface } from "../types/post.interface";
 
 export const getPosts = createAction("[Posts] Get Posts"); // we need to bring such a unique prefix ([Posts]) because all action is global
+export const getPostsSuccess = createAction("[Posts] Get Posts Success", props<{ posts: PostInterface[] }>());
+export const getPostsFailure = createAction("[Posts] Get Posts Failure", props<{ error: string }>());
--- src/app/posts/stores/reducers.ts
+++ src/app/posts/stores/reducers.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from "@ngrx/store";
 import { PostStateInterface } from "../types/postState.interface";
-import { getPosts } from "./actions";
+import { getPosts, getPostsFailure, getPostsSuccess } from "./actions";
 
 export const initialState: PostStateInterface = {
     isLoading: false,
@@ -13,5 +13,19 @@ export const reducers = createReducer(
     on(getPosts, (state) => {
         // We want add a loading indicator when we get posts
         return {...state, isLoading: true}
+    }),
+    on(getPostsSuccess, (state, action) => {
+        return {
+            ...state,
+            isLoading: false,
+            posts: action.posts
+        }
+    }),
+    on(getPostsFailure, (state, action) => {
+        return {
+            ...state,
+            isLoading: false,
+            error: action.error
+        }
     })
 )
