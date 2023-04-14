3f8006c30bc204eb58826707ae59bcf2feafb74b
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-04-14 08:51:04 +0700

    Ngrx: generate posts component

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostsComponent
+  ],
   imports: [
     CommonModule
   ]
